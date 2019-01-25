875353a6598896052b570c987e46402d5b49d291
tadangthanh81 <tadangthanh81@gmail.com>
2019-01-25 14:20:35 +0700

    first commit

---
+++ src/main/java/gdp5/team2/testingsys/Team2TestingSystemApplication.java
@@ -0,0 +1,14 @@
+package gdp5.team2.testingsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2TestingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team2TestingSystemApplication.class, args);
+	}
+
+}
+
